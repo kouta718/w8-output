adef7282613389a6466e9c9c2b2265a6f41550a4
kouta718 <qooron1927@gmail.com>
2025-03-03 05:24:25 +0900

    3/3-5:24am

--- login‐foam/script.js
+++ login‐foam/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const button = document.getElementById("button")
+
+// ローカル保存
+button.addEventListener("click", () => {
+    localStorage.setItem("email", JSON.stringify(email.value));
+    localStorage.setItem("password", JSON.stringify(password.value));
+})
+
+// 自動入力
+email.value = JSON.parse(localStorage.getItem("email"));
+password.value = JSON.parse(localStorage.getItem("password"));
